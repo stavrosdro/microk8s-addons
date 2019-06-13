6c547c8452c760dd417c8ec4b18b5e81641b4075
Stavros Droutsas <stavrosdro@gmail.com>
2019-06-13 11:51:27 +0300

    fixed ssh for host machine.

--- microk8s-webconsole/server/src/core/services/shell/shell.service.ts
+++ microk8s-webconsole/server/src/core/services/shell/shell.service.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { promisify } from 'util';
 import { exec } from 'child_process';
+import { Client } from 'ssh2'
 
 const exec_promise = promisify(exec);
 
+const pass = 'mypassword';
+let username = 'myusername';
+let host = '172.17.0.1';
+
+function execute_over_ssh(cmd: string): Promise<{ stdout: string; stderr: string; }> {
+  return new Promise<{ stdout: string; stderr: string; }>((resolve, reject) => {
+    let conn = new Client();
+    conn.on('ready', function () {
+      let stdout = "", stderr = "";
+      conn.exec(cmd, function (err, stream) {
+        if (err) reject(err);
+        stream.on('close', function (code, signal) {
+          resolve({ stdout, stderr });
+          conn.end();
+        }).on('data', function (data) {
+          stdout += data.toString('utf8');
+        }).stderr.on('data', function (data) {
+          stderr += data.toString('utf8');
+        });
+      });
+
+    }).connect({ host, port: 22, username, password: pass });
+  });
+}
+
 @Injectable()
 export class ShellService {
-
-    async execCommand(cmd: string, password?: string): Promise<string> {
-        if (password){
-            // TODO: Possibly remove
-            const command = `echo ${password} | sudo -S ${cmd}`;
-            const { stdout, stderr } = await exec_promise(command);
-            return stdout;
-        } else {
-            const command = `${cmd}`;
-            const { stdout, stderr } = await exec_promise(command);
-            return stdout;
-        }
+    async execCommand(cmd: string, password?: string): Promise<string> { 
+        const command = `echo '${pass}' | sudo -S ${cmd}`;
+        const { stdout, stderr } = await execute_over_ssh(command);
+        return stdout;
     }
 }
